b98786e8c621d88090bbab961ecbb860d38458a4
iampearlep <damilolaprincess4@gmail.com>
2023-03-11 23:52:26 +0100

    update components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+//import App from './App'
 import './index.css'
+import Root from './routes/Root'
+import About from './routes/About'
+import ErrorPage from './ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/about',
+    element: <About />
+  }
+ 
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </React.StrictMode>,
 )
