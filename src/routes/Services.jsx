21baf8b42864684138d3a75568fd82ada6542986
iampearlep <damilolaprincess4@gmail.com>
2023-03-12 00:30:40 +0100

    update services

--- src/routes/Services.jsx
+++ src/routes/Services.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>
+   
       <div className='bg-black text-white'>
       Our Services
     </div>
-    </div>
+    
   )
 }
 
