a625860a542335573e3901865491ed5d213a58a1
iampearlep <damilolaprincess4@gmail.com>
2023-03-12 01:29:05 +0100

    add new images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import AboutImg from '../assets/about-bg.png'
+import Spoon from '../assets/spoon.d079f43fbcf509908d3865293080795a.svg'
+import Knife from '../assets/knife.png'
+import GImg from '../assets/G.png'
 const About = () => {
   return (
     <section>
@@ -10,9 +13,34 @@ const About = () => {
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat'
         }
-      }>
-        <h1 className='text-white'>Hiiiii</h1>
-      </div>
+      } className='text-white'>
+        <div>
+          <div>
+            <h2>About Us</h2>
+            <img src={Spoon} alt="" />
+            <p>Lorem Ipsum Dolor Sit Amet, 
+              Consectetur Adipiscing Elit. 
+              Quis Pharetra Adipiscing Ultrices
+              Vulputate Posuere Tristique. In Sed 
+              Odio Nec Aliquet Eu Proin Mauris Et.</p>
+              <button>Know more</button>
+          </div>
+          <div>
+            <img src={GImg} alt="" />
+            <img src={Knife} alt="" />
+          </div>
+          <div>
+          <h2>Our History</h2>
+            <img src={Spoon} alt="" />
+            <p>Lorem Ipsum Dolor Sit Amet, 
+              Consectetur Adipiscing Elit. 
+              Quis Pharetra Adipiscing Ultrices
+              Vulputate Posuere Tristique. In Sed 
+              Odio Nec Aliquet Eu Proin Mauris Et.</p>
+              <button>Know more</button>
+          </div>
+          </div>
+        </div>
     </section>
   )
 }
