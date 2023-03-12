850b9445cfa7412d6210ea18e71784fb0d9ef923
iampearlep <damilolaprincess4@gmail.com>
2023-03-12 01:32:36 +0100

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className='w-11/12 md:w-2/5 md:mt-20'>
       <h4 className='text-2xl font-semibold'>Chase The New Flavour</h4>
       <img src={Spoon} alt="" />
-      <h1 className='mt-12 text-7xl text-primary font-bold w-11/12 mb-7'>THE KEY TO FINE DINING</h1>
+      <h1 className='mt-12 text-8xl text-primary font-bold w-12/12 mb-7'>The Key To Fine Dining</h1>
       <p className='mb-6'>Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus</p>
       <button className='bg-primary text-black py-3 px-5 font-semibold'>Explore Menu</button>
       </div>
