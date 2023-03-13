6e9067c6e00a1fb2048e43ed96881d55ae4716b1
iampearlep <damilolaprincess4@gmail.com>
2023-03-13 23:53:03 +0100

    update chef

--- src/components/Chef.jsx
+++ src/components/Chef.jsx
@@ -15,25 +15,25 @@ const Chef = () => {
         backgroundRepeat: 'no-repeat',
       }
     } className='relative h-[auto]'>
-      <div className='flex flex-col md:flex-row text-white py-32 w-11/12 justify-center items-center mx-auto space-x-10'>
+      <div className='flex flex-col md:flex-row text-white py-32 w-11/12 justify-center items-center mx-auto space-x-20'>
         <div className='w-1/2'>
         <img src={ChefImg} alt="" />
         </div>
         <div className='w-1/2'>
-          <h3>Chef’s Word</h3>
+          <h3 className='text-2xl font-semibold'>Chef’s Word</h3>
           <img src={Spoon} alt="" />
-          <h2>What We Believe In</h2>
+          <h2 className='text-primary text-6xl font-semibold mt-4'>What We Believe In</h2>
          
-          <div className='my-20 flex flex-row'> 
+          <div className='my-16 flex flex-row'> 
           <img className='absolute w-10' src={CommImg} alt="" />
           <p className='w-11/12 ml-auto text-xl'>Lorem Ipsum Dolor Sit Amet, 
             Consectetur Adipiscing Elit Auctor Sit Auctor 
             Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris
             Eget Sit. Nulla Scelerisque Scelerisque Congue Ac Consequat,
             Aliquam Molestie Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.</p></div>
-            <h3>Kevin Luo</h3>
-            <p>Chef & Founder</p>
-            <img src={Sign} alt="" />
+            <h3 className='text-primary text-4xl font-semibold mb-3'>Kevin Luo</h3>
+            <p className='text-xl mb-8'>Chef & Founder</p>
+            <img className='w-2/5' src={Sign} alt="" />
         </div>
       </div>
     </div>
