3eabf3c519fb9250372052020dfdc46d5f7c4d86
iampearlep <damilolaprincess4@gmail.com>
2023-03-15 21:54:50 +0100

    update awards

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -7,25 +7,25 @@ const Awards = () => {
   const awardData =[
     {
       id: 1,
-      image: '../../public/images/award-2.svg',
+      image: '../assets/award-3.svg',
       name:  'Bib Gourmond',
       text: 'Lorem ipsum dolor sit amet, consectetur.'
     },
     {
       id: 2,
-      image: '../../public/images/award-1.svg',
+      image: '../../assets/award-1.svg',
       name:  'Rising Star',
       text: 'Lorem ipsum dolor sit amet, consectetur.'
     },
     {
       id: 3,
-      image: '../../public/images/award-5.svg',
+      image: '../assets/award-5.svg',
       name:  'AA Hospitality',
       text: 'Lorem ipsum dolor sit amet, consectetur.'
     },
     {
       id: 4,
-      image: '../../public/images/award-3.svg',
+      image: '../assets/award-3.svg',
       name:  'Outstanding Chef',
       text: 'Lorem ipsum dolor sit amet, consectetur.'
     },
