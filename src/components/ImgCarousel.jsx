7635ebbdcd6f0551bbdd23d4e872a4ac1b5518a2
iampearlep <damilolaprincess4@gmail.com>
2023-04-14 04:29:55 +0100

    update insta, add new component and package

---
+++ src/components/ImgCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImgCarousel = () => {
+  return (
+    <div className='text-white'>
+        Hiiiiii
+    </div>
+  )
+}
+
+export default ImgCarousel
--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import InstaImg from '../assets/insta-bg.png'
 import Spoon from '../assets/spoon.d079f43fbcf509908d3865293080795a.svg'
-import Photo1 from '../assets/01.jpg'
+// import Photo1 from '../assets/01.jpg'
+import ImgCarousel from './ImgCarousel'
 const Insta = () => {
   return (
-    <div></div>
-    // <div style={
-    //   {
-    //     backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), 
-    //     url(${InstaImg})`,
-    //     backgroundPosition: 'center',
-    //     backgroundSize: 'cover',
-    //     backgroundRepeat: 'no-repeat',
-    //   }
-    // } >
-    //  <div>
-    //   <div className='text-white'>
-    //     <h4>Instagram</h4>
-    //     <img src={Spoon} alt="" />
-    //     <h3>Photo Gallery</h3>
-    //     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
-    //     <button className='bg-primary text-black py-2 px-7 text-lg font-semibold'>Know More</button>
-    //   </div>
-    //   <div>
-    //     <img src={Photo1} alt="" />
-    //   </div>
-    //  </div>
-    // </div>
+    <div style={
+      {
+        backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), 
+        url(${InstaImg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }
+    } >
+     <div>
+      <div className='text-white'>
+        <h4>Instagram</h4>
+        <img src={Spoon} alt="" />
+        <h3>Photo Gallery</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button className='bg-primary text-black py-2 px-7 text-lg font-semibold'>Know More</button>
+      </div>
+      <div>
+        <ImgCarousel />
+      </div>
+     </div>
+    </div>
   )
 }
 
