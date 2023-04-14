b6512dc5096be73b80818e30ba2b7d574795f6cc
iampearlep <damilolaprincess4@gmail.com>
2023-04-15 00:56:35 +0100

    update imgcarousel and insta

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import InstaImg from '../assets/insta-bg.png'
 import Spoon from '../assets/spoon.d079f43fbcf509908d3865293080795a.svg'
-// import Photo1 from '../assets/01.jpg'
 import ImgCarousel from './ImgCarousel'
 const Insta = () => {
   return (
@@ -14,7 +13,7 @@ const Insta = () => {
         backgroundRepeat: 'no-repeat',
       }
     } >
-     <div>
+     <div className='flex flex-row'>
       <div className='text-white'>
         <h4>Instagram</h4>
         <img src={Spoon} alt="" />
@@ -22,7 +21,7 @@ const Insta = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
         <button className='bg-primary text-black py-2 px-7 text-lg font-semibold'>Know More</button>
       </div>
-      <div>
+      <div className='w-2/3'>
         <ImgCarousel />
       </div>
      </div>
