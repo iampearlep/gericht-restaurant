ea39ef754f76a60499be958e0f6ea671e040a0d3
iampearlep <damilolaprincess4@gmail.com>
2023-04-15 03:46:34 +0100

    add video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,6 +4,9 @@ const Video = () => {
   return (
     <div>
        <div className='w-full'>
+       {/* <video className='w-full h-[400px]' controls>
+      <source src="../assets/video.mp4" type="video/raw" />
+    </video> */}
       <iframe className='w-full h-[380px]' src="https://www.youtube.com/embed/WdWEMXnHBVI?controls=0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
     </div>
     </div>
