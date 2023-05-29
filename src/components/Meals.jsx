c284a388b914335caf5dba512d6839cf911fd37d
Princess Enuma <87270940+iampearlep@users.noreply.github.com>
2023-05-29 16:59:35 +0100

    Update Meals.jsx

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -13,15 +13,7 @@ const Meals= () => {
       setMeals(res2.data.meals);
     }
 
-    fetchData()
-    // axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    // .then(res => {
-    //   // console.log(response.data.categories[0].strCategory);
-    //   // console.log(res.data.categories);
-    //   setCategory(res.data.categories)
-    // })
-  }, []);
-
+    
 
 
   return (
@@ -54,21 +46,6 @@ const Meals= () => {
       )
     })}
    </div>
-     {/* <button key={data[0].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg'>{data[0].strCategory}</button>
-     <button key={data[1].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[1].strCategory}</button>
-     <button key={data[2].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[2].strCategory}</button>
-     <button key={data[3].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[3].strCategory}</button>
-     <button key={data[4].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[4].strCategory}</button>
-     <button key={data[5].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[5].strCategory}</button>
-     <button key={data[6].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[6].strCategory}</button>
-     <button key={data[7].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[7].strCategory}</button>
-     <button key={data[8].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[8].strCategory}</button>
-     <button key={data[9].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[9].strCategory}</button>
-     <button key={data[10].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[10].strCategory}</button>
-     <button key={data[11].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[11].strCategory}</button>
-     <button key={data[12].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[12].strCategory}</button>
-     <button key={data[13].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[13].strCategory}</button> */}
-     
        </div>
   )
 }
