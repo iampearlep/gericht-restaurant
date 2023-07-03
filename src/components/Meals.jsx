b06ce5a54237bd61d94d3158baae3fb6095b60ed
iampearlep <damilolaprincess4@gmail.com>
2023-07-03 05:38:11 +0100

    update meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -9,20 +9,18 @@ const Meals= () => {
     async function fetchData () {
       const res1 = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
       setCategory(res1.data.categories);
-      const res2 = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken');
+      const res2 = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef');
       setMeals(res2.data.meals);
     }
 
     fetchData()
-    // axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    // .then(res => {
-    //   // console.log(response.data.categories[0].strCategory);
-    //   // console.log(res.data.categories);
-    //   setCategory(res.data.categories)
-    // })
   }, []);
 
-
+  // const FetchMealData = (e) => {
+  //   // axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${}`);
+  //   console.log('button was clicked');
+  //   console.log(e.target.value)
+  // }
 
   return (
     <div className='bg-primaryBlack  pt-14'>
@@ -35,17 +33,17 @@ const Meals= () => {
     {category?.map((item) => {
       return (
         <div key={item.idCategory} className=''>
-         <button className='bg-primary py-2 font-bold px-10'>
+         <button className='bg-primary py-2 font-bold px-10' onClick={() => setCategory(category?.strCategory)}>
          {item?.strCategory}
          </button>
         </div>
       )
      })}
     </div>
-   <div className='flex flex-col md:flex-row md:flex-wrap justify-center items-center mx-auto w-11/12 gap-x-10 gap-y-4'>
+   <div className='flex flex-col md:grid md:grid-cols-4 mx-auto w-full px-5 gap-x-10 gap-y-4'>
     {meals?.map((meal) => {
       return(
-        <div key={meal.idMeal} className='md:w-3/12 border border-primary mt-10'>
+        <div key={meal.idMeal} className=' border border-primary mt-10'>
           <div className=''>
           <img src={meal.strMealThumb} alt="" />
           </div>
@@ -54,21 +52,6 @@ const Meals= () => {
       )
     })}
    </div>
-     {/* <button key={data[0].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg'>{data[0].strCategory}</button>
-     <button key={data[1].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[1].strCategory}</button>
-     <button key={data[2].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[2].strCategory}</button>
-     <button key={data[3].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[3].strCategory}</button>
-     <button key={data[4].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[4].strCategory}</button>
-     <button key={data[5].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[5].strCategory}</button>
-     <button key={data[6].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[6].strCategory}</button>
-     <button key={data[7].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[7].strCategory}</button>
-     <button key={data[8].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[8].strCategory}</button>
-     <button key={data[9].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[9].strCategory}</button>
-     <button key={data[10].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[10].strCategory}</button>
-     <button key={data[11].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[11].strCategory}</button>
-     <button key={data[12].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[12].strCategory}</button>
-     <button key={data[13].idCategory} className='bg-primary px-6 py-2 font-semibold text-lg' >{data[13].strCategory}</button> */}
-     
        </div>
   )
 }
